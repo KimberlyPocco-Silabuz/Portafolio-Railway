19b1eccf699a259e40c31e9137d1d6cca9d4c872
KimberlyPocco <57849420+KimberlyPocco@users.noreply.github.com>
2022-12-10 17:53:45 +0000

    modificacion de rutas railway

--- blog/urls.py
+++ blog/urls.py
@@ -6,13 +6,13 @@ from blog.views import feed,profile,register,create_project,index,deleteProject
  
 urlpatterns = [
 	path('', index, name='index'),
-	path('feed/', feed.as_view(), name='feed'),
+	path('accounts/feed/', feed.as_view(), name='feed'),
 	path('accounts/profile/', profile, name='profile'),
 	path('profile/<str:username>/', profile, name='profile'), #esta es user y proyecto
 	path('register/', register, name='register'),
 	path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-	path('logout/', LogoutView.as_view(template_name='index.html'), name='logout'),
-	path('proyecto/', create_project, name='proyecto'),
-	path('delete/<int:id>', deleteProject, name="delete"),
+	path('accounts/logout/', LogoutView.as_view(template_name='index.html'), name='logout'),
+	path('accounts/proyecto/', create_project, name='proyecto'),
+	path('accounts/delete/<int:id>', deleteProject, name="delete"),
 	
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
