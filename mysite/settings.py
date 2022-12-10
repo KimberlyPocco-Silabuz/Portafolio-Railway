fbad4e089fa24c1362dfffc2ad1b8b22d8d08e87
KimberlyPocco <57849420+KimberlyPocco@users.noreply.github.com>
2022-12-10 17:28:26 +0000

    cambios app settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'blog'
+   # "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
@@ -120,7 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
+DISABLE_COLLECTSTATIC= 0
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
